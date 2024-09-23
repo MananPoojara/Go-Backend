462841531611d31ffc83636a86d01509d9fade4e
MananPoojara <billybutcher215@gmail.com>
2024-09-23 18:00:28 +0530

    Update Commit

--- Conditional/if-else/if-else.go
+++ Conditional/if-else/if-else.go
@@ -10,10 +10,21 @@ func main() {
 	maxmessageLen := 20
 	fmt.Println("Trying To send Message :", messageLen, "Maximum Message is :", maxmessageLen)
 
+	// Go Always Make syntax Sorter and make out programming faster
+
+	fmt.Println("******The initial statement of an if block*******")
+	//if INITIAL_STATEMENT; CONDITION { }
+
+	//Normal Syntax
 	if messageLen <= maxmessageLen {
 		fmt.Println("Message Sent")
 	} else {
 		fmt.Println("Message can't Sent")
+	}
+
+	//Go Mode Syntax
+	if mxmsg := maxmessageLen; messageLen <= mxmsg {
+		fmt.Println("Message Sent")
 	}
 
 }
